15d985ee4b28bb13190f82b3e997d2b4d6fdf457
Mellamomax <max.skold@hotmail.com>
2024-07-16 22:25:53 +0200

    Update football-lineup-card.js

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -4,58 +4,17 @@ class FootballLineupCard extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
             <style>
-                .field {
-                    background: url('/local/football-pitch-template.jpg') no-repeat center center;
-                    background-size: contain;
-                    width: 100%;
-                    height: 400px;
-                    position: relative;
-                }
-                .player {
-                    position: absolute;
-                    color: white;
-                    background-color: rgba(0, 0, 0, 0.5);
-                    padding: 5px;
-                    border-radius: 5px;
-                    text-align: center;
+                .card {
+                    padding: 16px;
+                    font-size: 20px;
                 }
             </style>
-            <div class="field">
-                <div class="info"></div>
-            </div>
+            <div class="card">Hello World</div>
         `;
-    }
-
-    set hass(hass) {
-        const entity = hass.states[this.config.entity];
-        if (!entity) {
-            this.shadowRoot.querySelector('.info').innerHTML = 'Entity not found';
-            return;
-        }
-        const attributes = entity.attributes;
-        const formation = attributes.formation;
-        const coach = attributes.coach;
-        const startingXI = attributes["starting XI"];
-
-        const field = this.shadowRoot.querySelector('.field');
-        const info = this.shadowRoot.querySelector('.info');
-        info.innerHTML = `
-            <p>Formation: ${formation}</p>
-            <p>Coach: ${coach}</p>
-        `;
-        
-        startingXI.forEach(player => {
-            const playerDiv = document.createElement('div');
-            playerDiv.className = 'player';
-            playerDiv.style.left = `${player.grid.split(':')[1] * 10}%`;
-            playerDiv.style.top = `${player.grid.split(':')[0] * 10}%`;
-            playerDiv.innerHTML = `${player.name} (${player.number})`;
-            field.appendChild(playerDiv);
-        });
     }
 
     getCardSize() {
-        return 3;
+        return 1;
     }
 }
 
