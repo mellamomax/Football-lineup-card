3c981dcc014c2ad9381c4ee9e46393687f666953
Mellamomax <max.skold@hotmail.com>
2024-07-16 02:22:19 +0200

    Update football-lineup-card.js

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -62,3 +62,20 @@ class FootballLineupCard extends HTMLElement {
 }
 
 customElements.define('football-lineup-card', FootballLineupCard);
+
+
+// code to show the card in HA card-picker
+const FootballLineupCardDescriptor = {
+    type: 'football-lineup-card', // Must match the type you use in your YAML configuration
+    name: 'Football Lineup Card', // Friendly name for the card picker
+    description: 'A custom card to show lineup', // Short description
+    preview: false, // Optional: Set to true to show a preview in the picker
+    documentationURL: 'https://justpaste.it/38sr8' // Optional: Link to your documentation
+};
+
+// Ensure window.customCards is initialized
+window.customCards = window.customCards || [];
+
+// Add your card to the customCards array
+window.customCards.push(FootballLineupCardDescriptor);
+
