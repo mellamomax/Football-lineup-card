fbbc1e836341b5e7ee0c545eff98e167241b2ba4
Mellamomax <max.skold@hotmail.com>
2024-07-17 00:42:31 +0200

    Update football-lineup-card.js

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -80,7 +80,7 @@ class FootballLineupCard extends HTMLElement {
                 }
                 .field {
                     background: url('/local/football-pitch-template.jpg') no-repeat center center;
-                    background-size: contain;
+                    background-size: cover;
                     width: 100%;
                     height: 350px;
                     position: relative;
