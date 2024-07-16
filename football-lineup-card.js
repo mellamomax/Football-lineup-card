ef77853e2396311b351005cb8e01c356eec45116
Mellamomax <max.skold@hotmail.com>
2024-07-16 12:57:21 +0200

    sd

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -2,39 +2,48 @@ class FootballLineupCard extends HTMLElement {
     setConfig(config) {
         this.config = config;
         this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <style>
+                .field {
+                    background: url('/local/football-pitch-template.jpg') no-repeat center center;
+                    background-size: contain;
+                    width: 100%;
+                    height: 400px;
+                    position: relative;
+                }
+                .player {
+                    position: absolute;
+                    color: white;
+                    background-color: rgba(0, 0, 0, 0.5);
+                    padding: 5px;
+                    border-radius: 5px;
+                    text-align: center;
+                }
+            </style>
+            <div class="field">
+                <div class="info"></div>
+            </div>
+        `;
     }
 
     set hass(hass) {
         const entity = hass.states[this.config.entity];
+        if (!entity) {
+            this.shadowRoot.querySelector('.info').innerHTML = 'Entity not found';
+            return;
+        }
         const attributes = entity.attributes;
-
         const formation = attributes.formation;
         const coach = attributes.coach;
         const startingXI = attributes["starting XI"];
-        const substitutes = attributes.substitutes;
 
-        const style = `
-            .field {
-                background: url('/hacsfiles/football-lineup-card/img/football-pitch-template.jpg') no-repeat center center;
-                background-size: contain;
-                width: 100%;
-                height: 400px;
-                position: relative;
-            }
-            .player {
-                position: absolute;
-                color: white;
-                background-color: rgba(0, 0, 0, 0.5);
-                padding: 5px;
-                border-radius: 5px;
-                text-align: center;
-            }
+        const field = this.shadowRoot.querySelector('.field');
+        const info = this.shadowRoot.querySelector('.info');
+        info.innerHTML = `
+            <p>Formation: ${formation}</p>
+            <p>Coach: ${coach}</p>
         `;
-
-        const field = document.createElement('div');
-        field.className = 'field';
-        field.style = style;
-
+        
         startingXI.forEach(player => {
             const playerDiv = document.createElement('div');
             playerDiv.className = 'player';
@@ -43,17 +52,6 @@ class FootballLineupCard extends HTMLElement {
             playerDiv.innerHTML = `${player.name} (${player.number})`;
             field.appendChild(playerDiv);
         });
-
-        this.shadowRoot.innerHTML = `
-            <style>${style}</style>
-            <div class="field">
-                <div class="info">
-                    <p>Formation: ${formation}</p>
-                    <p>Coach: ${coach}</p>
-                </div>
-                ${field.innerHTML}
-            </div>
-        `;
     }
 
     getCardSize() {
