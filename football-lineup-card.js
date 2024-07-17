2ab3c4eb66159ad714b40f2aa525c2f25d449713
Mellamomax <max.skold@hotmail.com>
2024-07-18 01:23:49 +0200

    Update football-lineup-card.js

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -89,8 +89,8 @@ class FootballLineupCard extends HTMLElement {
                 .player-circle {
                     background-color: rgba(255,255,255);
     		    border-radius: 50%; /* Ensures the shape is a circle */
-    		    width: 20vw; /* Diameter of the circle */
-    		    height: 20vw; /* Diameter of the circle */
+    		    width: 5vw; /* Diameter of the circle */
+    		    height: 5vw; /* Diameter of the circle */
     		    display: flex;
    		    justify-content: center;
     		    align-items: center;
