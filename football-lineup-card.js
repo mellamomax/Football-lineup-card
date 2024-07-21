68678e21960e9f52fb2615e594f8126b3687610e
Mellamomax <max.skold@hotmail.com>
2024-07-22 01:26:00 +0200

    Update football-lineup-card.js

--- football-lineup-card.js
+++ football-lineup-card.js
@@ -72,13 +72,35 @@ class FootballLineupCard extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
             <style>
-                .field {
-                    background: url('/local/football-pitch-template.png') no-repeat center center;
-                    background-size: cover;
-                    width: 100%;
-   		    padding-top: 66.66%; /* Aspect ratio 3:2 */
-                    position: relative;
+                @font-face {
+                    font-family: 'Tolyer';
+                    src: url('/local/fonts/Tolyer-Regular-no.1.ttf') format('truetype');
                 }
+
+                .card {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    position: relative;
+                    padding-bottom: 2vw; /* Add space below the card */
+                }
+
+                .field-container {
+                    position: relative;
+                    width: 100%;
+                    padding-top: 66.66%; /* Aspect ratio 3:2 */
+                }
+
+                .field {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background: url('/local/football-pitch-template.png') no-repeat center center;
+                    background-size: contain;
+                }
+
                 .players {
                     position: absolute;
                     top: 0;
@@ -86,46 +108,45 @@ class FootballLineupCard extends HTMLElement {
                     width: 100%;
                     height: 100%;
                 }
-		.player-container {
- 		    position: absolute;
-   		    transform: translate(-50%, -50%); /* Center the container */
-  		    text-align: center;
-		    #width: 2vw;
-		    #height: 2vw;
-		}
+
+                .player-container {
+                    position: absolute;
+                    transform: translate(-50%, -50%); /* Center the container */
+                    text-align: center;
+                }
+
                 .player-circle {
-                    background-color: rgba(255,255,255);
-    		    border-radius: 50% !important; /* Ensures the shape is a circle */
-    		    width: 6vw; /* Diameter of the circle */
-    		    height: 6vw; /* Diameter of the circle */
-    		    display: flex;
-   		    justify-content: center;
-    		    align-items: center;
-		    position: relative;
-		}
+                    background-color: rgba(255, 255, 255, 1);
+                    border-radius: 50%;
+                    width: 6vw; /* Diameter of the circle */
+                    height: 6vw; /* Diameter of the circle */
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    position: relative;
+                }
+
                 .player-circle img {
                     border-radius: 50%;
                     width: 5vw;
-		    height: 5vw;
-                    #height: 180%;
-	  	    object-fit: cover;
+                    height: 5vw;
+                    object-fit: cover;
                     object-position: top;
-		    position: absolute;
-		    z-index: 1; /* Ensure the image is on top */
+                    z-index: 1; /* Ensure the image is on top */
                 }
+
                 .player-name {
-                    text-align: center;
-                    font-size: 1.2vw;
-		    position: relative;
-                    top: 0.7vw;
-		    font-weight: 580;
-                    font-family: Tolyer;
-		    color: white; /* Color of the text */
+                    font-size: 1vw;
+                    font-family: 'Tolyer', sans-serif;
+                    color: white;
+                    margin-top: -1vw;
                 }
             </style>
             <div class="card">
-                <div class="field"></div>
-                <div class="players"></div>
+                <div class="field-container">
+                    <div class="field"></div>
+                    <div class="players"></div>
+                </div>
             </div>
         `;
     }
@@ -146,7 +167,6 @@ class FootballLineupCard extends HTMLElement {
             return;
         }
 
-        const field = this.shadowRoot.querySelector('.field');
         const players = this.shadowRoot.querySelector('.players');
         players.innerHTML = '';
 
@@ -157,7 +177,7 @@ class FootballLineupCard extends HTMLElement {
                 const playerContainer = document.createElement('div');
                 playerContainer.className = 'player-container';
                 playerContainer.style.left = `${((5 - position.y) / 5) * 100}%`; // Adjust for appropriate positioning and flip horizontally
-                playerContainer.style.bottom = `${((position.x / 5) * 100) - 25}%`;  // Adjust for appropriate positioning and move down
+                playerContainer.style.top = `${((position.x / 5) * 100) - 10}%`;  // Adjust for appropriate positioning and move down
 
                 const playerCircle = document.createElement('div');
                 playerCircle.className = 'player-circle';
@@ -173,7 +193,7 @@ class FootballLineupCard extends HTMLElement {
                 playerCircle.appendChild(playerImage);
                 playerContainer.appendChild(playerCircle);
                 playerContainer.appendChild(playerName);
-                field.appendChild(playerContainer);
+                players.appendChild(playerContainer);
             }
         });
     }
